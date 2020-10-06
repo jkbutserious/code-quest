58ad6478c0c718facac0f43022da58b31bc0decd
Jeff Kim <kim2jy@hotmail.com>
2020-10-06 11:12:42 -0700

    Successfully integrate firebase with project

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import formVisibleReducer from './form-visible-reducer';
 import quest-reducer from './quest-reducer';
 import { combineReducers } from 'redux';
+import { firestoreReducer } from 'redux-firestore';
 
 const rootReducer = combineReducers({
   formVisibleOnPage: formVisibleReducer,
   masterQuestList: quest-reducer
+  firestore: firestoreReducer
 });
 
 export default rootReducer;
